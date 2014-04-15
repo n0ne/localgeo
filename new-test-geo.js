510ea306ba20e56d1d7eb5b92ab03470d47a1ec4
none <alexander.podkidyshev@gmail.com>
2014-04-15 11:11:57 +0300

    Working reset markers, local search

--- new-test-geo.js
+++ new-test-geo.js
@@ -1,7 +1,6 @@
-// map = null
+
 
 allMarkers = new Meteor.Collection('markers');
-// allMarkers._ensureIndex({location: "2dsphere"});
 
 if (Meteor.isClient) {
 
@@ -19,7 +18,7 @@ if (Meteor.isClient) {
         var handle = Meteor.subscribe('markers');
         if(handle.ready()) {
           NProgress.done();
-          console.log("handle.ready(): total markers: " + markers.length);
+          // console.log("handle.ready(): total markers: " + markers.length);
           markersGroup.addLayers(markers);
           map.addLayer(markersGroup);
         } else {
@@ -32,26 +31,6 @@ if (Meteor.isClient) {
 
 
 
-  removeMarker = function(marker) {
-
-    map.removeLayer(markersGroup);
-
-    // markers.forEach(function(_id) {
-    //   var marker = markers[_id];
-
-    //   markers.forEach(function(m) {
-    //     if(m.options._id !== _id) {
-    //       markersGroup.removeLayer(m);
-    //     }
-    //   });
-
-    // });
-
-
-  }
-
-  // var activeIcon;
-
     activeIcon = L.icon({
       iconUrl: "packages/leaflet/images/marker-icon.png",
       shadowUrl: "packages/leaflet/images/marker-shadow.png",
@@ -62,7 +41,7 @@ if (Meteor.isClient) {
   
   Template.map.rendered = function () {
 
-    console.log("Template.map.rendered");
+    // console.log("Template.map.rendered");
 
 
     $(window).resize(function () {
@@ -73,7 +52,7 @@ if (Meteor.isClient) {
 
     }).resize();
 
-    console.log("Init map");
+    // console.log("Init map");
     map = L.map($('#map_canvas')[0], {
       scrollWheelZoom: true,
       doubleClickZoom: false,
@@ -93,7 +72,8 @@ if (Meteor.isClient) {
       maxClusterRadius: 100,
       spiderfyOnMaxZoom: true,
       showCoverageOnHover: false,
-      zoomToBoundsOnClick: true
+      zoomToBoundsOnClick: true,
+      chunkedLoading: true
     });
 
     // allMarkers.find({location: {$near: { $geometry: { type: "Point", coordinates: [-122.41544999999999, 37.7745897] }, $maxDistance: 50 }}}).observe({
@@ -107,48 +87,22 @@ if (Meteor.isClient) {
           icon: activeIcon
         }).on('click', function(e) {
           Session.set('location', [lng, lat]);
-          console.log("Marker clicked. Location: " + Session.get('location'));
-
-          // map.panTo(e.target.getLatLng());
-          // console.log("Total markers array size: " + markers.length);
-          // console.log(e);
-          // removeMarker(e.target.options._id);
+          // console.log("Marker clicked. Location: " + Session.get('location'));
 
           markersGroup.clearLayers(markers);
 
         });
-
-        // map.addLayer(marker);
-
-        // markersGroup.addLayer(marker);
-
         markers.push(marker);
-        // markers[marker.options._id] = marker;
-         // console.log(markers[marker.options._id]);
-
-        // map.addLayer(markersGroup);
-
-        // console.log("Lat: " + m.location.coordinates[1] + ", Lng: " + m.location.coordinates[0]);
-        // var marker = L.Marker([m.location.coordinates[1], m.location.coordinates[0]], {
-        //   _id: m._id,
-        //   icon: activeIcon
-        // });
-
-        // map.addLayer(marker);
+        
 
       }
     });
-    // if(DDP._allSubscriptionsReady()) {
-    //   console.log("DDP: total markers: " + markers.length);
-    //   markersGroup.addLayers(markers);
-    //   map.addLayer(markersGroup);
-    // }
-    console.log("Total markers: " + markers.length);
-    Session.set('map', true)
+    // console.log("Total markers: " + markers.length);
+    // Session.set('map', true)
   }
 
   Template.map.created = function() {
-    console.log("Created map. Total markers: " + markers.length);
+    // console.log("Created map. Total markers: " + markers.length);
   }
 
   Template.map.destroyed = function () {
@@ -159,14 +113,23 @@ if (Meteor.isClient) {
   Template.map.events({
   "click #clear": function(e, t) {
     e.preventDefault();
-
-    // map.removeLayer(markersGroup);
-
-    // Session.set('clear', true);
     markersGroup.clearLayers(insideMarkers);
 
     markersGroup.addLayers(markers);
     map.addLayer(markersGroup);
+  },
+
+  "click #100m": function(e, t) {
+    e.preventDefault();
+    Session.set('radius', 100);
+  },
+  "click #500m": function(e, t) {
+    e.preventDefault();
+    Session.set('radius', 500);
+  },
+  "click #1000m": function(e, t) {
+    e.preventDefault();
+    Session.set('radius', 1000);
   }
 });
 
@@ -176,12 +139,15 @@ if (Meteor.isClient) {
 
     Deps.autorun(function() {
 
+        var radius = Session.get('radius') ? Session.get('radius') : 100 ;
         var location  = Session.get('location');
-        console.log("From Deps.autorun: "+ location);
+        // console.log("From Deps.autorun: "+ location);
+
+        // markersGroup.clearLayers(insideMarkers);
 
         if(location) {
           insideMarkers =[];
-          console.log("From Deps.autorun. If statement.");
+          // console.log("From Deps.autorun. If statement.");
 
           var nearMarkers = allMarkers.find({
             location: {
@@ -190,73 +156,35 @@ if (Meteor.isClient) {
                   type: "Point",
                   coordinates: location
                 },
-                $maxDistance: 100
+                $maxDistance: radius
               }
             }
           });
 
-          console.log("Total near markers " + nearMarkers.count());
+          // console.log("Total near markers " + nearMarkers.count());
 
           nearMarkers.forEach(function(m) {
             var lat = m.location.coordinates[1];
             var lng = m.location.coordinates[0];
 
-            console.log("Marker LatLng: " + [lat, lng]);
+            // console.log("Marker LatLng: " + [lat, lng]);
 
             var marker = new L.Marker([lat, lng], {
               _id: m._id,
               icon: activeIcon
-            });                                   // have to add 'click' action
+            }).on('click', function(e) {
+                Session.set('location', [lng, lat]);
+              });                                  
 
             insideMarkers.push(marker);
           });
 
-          console.log("insideMarkers.length: " + insideMarkers.length);
+          // console.log("insideMarkers.length: " + insideMarkers.length);
 
           markersGroup.clearLayers(markers);
           markersGroup.addLayers(insideMarkers);
           map.addLayer(markersGroup);
-
-
         }
-
-        // if(location) {
-
-          // var nearMarkers = allMarkers.find({
-          //   location: {
-          //     $near: {
-          //       $geometry: {
-          //         type: "Point",
-          //         coordinates: location
-          //       },
-          //       $maxDistance: 100
-          //     }
-          //   }
-          // });
-
-          // console.log("Total near markers " + nearMarkers.count());
-
-        //   nearMarkers.forEach(function(m) {
-        //     var lat = m.location.coordinates[1];
-        //     var lng = m.location.coordinates[0];
-
-        //     var marker = new L.Marker([lat, lng], {
-        //       _id: m._id,
-        //       icon: activeIcon
-        //     }).on('click', function(e) {
-        //       Session.set('location', [lng, lat]);
-        //       markersGroup.clearLayers(insideMarkers);
-        //     });
-
-        //     insideMarkers.push(marker);
-        //   });
-
-        //   markersGroup.addLayers(insideMarkers);
-
-
-
-
-        // }
     });
 
   });
